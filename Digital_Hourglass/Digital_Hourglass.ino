bdc389672c474cfeb4420e94ded182d282fa5a50
Karla Lopez <klo.lopez@gmail.com>
2013-06-29 19:38:25 -0300

    Comments
    Just some comments

--- Digital_Hourglass/Digital_Hourglass.ino
+++ Digital_Hourglass/Digital_Hourglass.ino
@@ -3,7 +3,7 @@ unsigned long previousTime = 0;
 int switchState = 0;
 int prevSwitchState = 0;
 int led = 2;
-long interval = 10000;
+long interval = 10000; // 10 seconds
 
 void setup() {
   for(int x = 2; x<8; x++) {
@@ -19,13 +19,13 @@ void loop() {
     previousTime = currentTime;
     digitalWrite(led, HIGH);
     led++;
-    if(led == 7) {
+    if(led == 7) { // nothing happens at the end, but if you want to do something when the time ends, here is the place
     }
   }
   switchState = digitalRead(switchPin);
   if(switchState != prevSwitchState) {
     for(int x = 2; x<8; x++) {
-      digitalWrite(x, LOW);
+      digitalWrite(x, LOW); // turn off all LEDs
     }
     led = 2;
     previousTime = currentTime;
--- Light_Theremin/Light_Theremin.ino
+++ Light_Theremin/Light_Theremin.ino
@@ -1,11 +1,11 @@
 int sensorValue;
 int sensorLow = 1023;
 int sensorHigh = 0;
-const int ledPin = 13; // This is an indicator that the sensor has finished calibrating, using the onboard led connected to pin 13.
+const int ledPin = 13; // we will set up an indicator that the sensor has finished calibrating, using the onboard led connected to pin 13.
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin, HIGH); // callibration beggins
   while (millis() < 5000) {
     sensorValue = analogRead(A0);
     if (sensorValue > sensorHigh) {
@@ -15,15 +15,16 @@ void setup() {
       sensorLow = sensorValue;
     }
   }
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, LOW); // calibration ends
 }
 
 void loop() {
   sensorValue = analogRead(A0);
-  int pitch = map(sensorValue,sensorLow,sensorHigh, 0, 4000); 
-  tone (8,pitch,2000);
-  delay(10);
+  int pitch = map(sensorValue,sensorLow,sensorHigh, 0, 4000); // the last 2 values here change the frequencies
+  tone (8,pitch,2000); // the variables are: pin, sound frequency and how long to play the note
+  delay(10); // for how long it will play
   Serial.print ("Sensor Value: ");
   Serial.print (sensorValue);
+  Serial.print ("  ");
 }
 
